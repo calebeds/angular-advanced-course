a63aa97e67218bb837f194244f7d21442e26dc5b
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2023-07-03 16:19:33 -0300

    use content child decorator to console inputs

--- au-input/src/app/lib/au-fa-input/au-fa-input.component.ts
+++ au-input/src/app/lib/au-fa-input/au-fa-input.component.ts
@@ -1,17 +1,30 @@
-import { Component, Input, OnInit } from "@angular/core";
+import {
+  Component,
+  ContentChild,
+  Input,
+  OnInit,
+  AfterContentInit,
+} from "@angular/core";
 
 @Component({
   selector: "au-fa-input",
   templateUrl: "./au-fa-input.component.html",
   styleUrls: ["./au-fa-input.component.css"],
 })
-export class AuFaInputComponent implements OnInit {
+export class AuFaInputComponent implements OnInit, AfterContentInit {
   @Input()
   icon: string;
+
+  @ContentChild("input")
+  input: HTMLInputElement;
 
   constructor() {}
 
   ngOnInit() {}
+
+  ngAfterContentInit(): void {
+    console.log("input", this.input);
+  }
 
   get classes(): unknown {
     const cssClasses = {};
