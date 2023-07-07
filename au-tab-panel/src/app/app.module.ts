e86ef9cbc5cea3ac07e1802557849929768170cf
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2023-07-07 18:55:16 -0300

    use ConentChildren in au-tab-panel to get title property from au-tab

--- au-tab-panel/src/app/app.module.ts
+++ au-tab-panel/src/app/app.module.ts
@@ -1,26 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 
-
-import { AppComponent } from './app.component';
-import {HttpClientModule} from "@angular/common/http";
-import { AuTabComponent } from './au-tab/au-tab.component';
-import { AuTabPanelComponent } from './au-tab-panel/au-tab-panel.component';
+import { AppComponent } from "./app.component";
+import { AuTabComponent } from "./au-tab/au-tab.component";
+import { AuTabPanelComponent } from "./au-tab-panel/au-tab-panel.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    AuTabComponent,
-    AuTabPanelComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpClientModule
-  ],
+  declarations: [AppComponent, AuTabComponent, AuTabPanelComponent],
+  imports: [BrowserModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
-  entryComponents: []
+  entryComponents: [],
 })
-export class AppModule { }
+export class AppModule {}
