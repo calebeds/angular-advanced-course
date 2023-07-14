507d9351bd8b220c972c547528eef029dc120566
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2023-07-14 14:08:58 -0300

    add tests which validate the initial behavior of tab container

--- au-tab-panel/src/app/app.component.spec.ts
+++ au-tab-panel/src/app/app.component.spec.ts
@@ -1,32 +1,55 @@
-import { TestBed, async } from '@angular/core/testing';
+import { ComponentFixture, TestBed, async } from "@angular/core/testing";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { AuTabPanelComponent } from "./au-tab-panel/au-tab-panel.component";
+import { DebugElement } from "@angular/core";
+import { AuTabComponent } from "./au-tab/au-tab.component";
+import { By } from "@angular/platform-browser";
 
-describe('AppComponent', () => {
+describe("AppComponent", () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let app: AppComponent;
+  let el: DebugElement;
+  let tabPanel: DebugElement;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent, AuTabPanelComponent, AuTabComponent],
     }).compileComponents();
   }));
 
-  it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    app = fixture.debugElement.componentInstance;
+    el = fixture.debugElement;
+    tabPanel = el.query(By.css("#tab-panel"));
+
+    fixture.detectChanges();
+  });
+
+  it("should create the app", async(() => {
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'app works!'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app works!');
+  it("should find only one tab inside the tab container", async(() => {
+    const tabs = tabPanel.queryAll(By.css(".tab"));
+
+    expect(tabs).toBeTruthy();
+    expect(tabs.length).toBe(1);
   }));
 
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('app works!');
+  it("should find the Contact tab button marked as active", async(() => {
+    const selectedButton = tabPanel.query(
+      By.css(".tab-panel-buttons li.selected")
+    ).nativeElement;
+
+    expect(selectedButton).toBeTruthy();
+    expect(selectedButton.textContent).toBe(" Contact ");
+  }));
+
+  it("should display the Contacts tab", async(() => {
+    const contactEmail = tabPanel.query(By.css(".contact-email"));
+
+    expect(contactEmail).toBeTruthy();
   }));
 });
