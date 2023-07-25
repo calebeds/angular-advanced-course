34dd97ca8df1caa660bfc7b6d245c09250005686
Calebe Dos Santos Oliveira <calebe.dso@gmail.com>
2023-07-25 15:30:33 -0300

    add parameters to animations and use lifecycle hooks

--- animations/src/app/animations.ts
+++ animations/src/app/animations.ts
@@ -1,8 +1,25 @@
-import { animation, style, animate } from "@angular/animations";
+import {
+  animation,
+  style,
+  animate,
+  trigger,
+  transition,
+  useAnimation,
+} from "@angular/animations";
 
-export const fadeIn = animation([
-  style({ opacity: 0 }),
-  animate("500ms", style({ opacity: 1 })),
+export const fadeIn = animation(
+  [style({ opacity: 0 }), animate("{{delay}}", style({ opacity: 1 }))],
+  { params: { delay: "1000ms" } }
+);
+
+export const fadeOut = animation(animate("{{delay}}", style({ opacity: 0 })), {
+  params: { delay: "1000ms" },
+});
+
+export const fadeInOut = trigger("fadeInOut", [
+  transition(
+    "void => *",
+    useAnimation(fadeIn, { params: { delay: "2000ms" } })
+  ),
+  transition("* => void", useAnimation(fadeOut)),
 ]);
-
-export const fadeOut = animation(animate("500ms", style({ opacity: 0 })));
--- animations/src/app/app.component.ts
+++ animations/src/app/app.component.ts
@@ -1,12 +1,17 @@
-import {Component} from '@angular/core';
+import { Component } from "@angular/core";
+import { fadeInOut } from "./animations";
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
+  animations: [fadeInOut],
 })
 export class AppComponent {
-
-
-
+  onAnimationStart(): void {
+    console.log("On Animation Start");
+  }
+  onAnimationEnd(): void {
+    console.log("On Animation End");
+  }
 }
